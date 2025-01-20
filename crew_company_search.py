a8a4f304ad15fa33f517e81c31002c2101dfb79d
gallus <gall.alain@gmail.com>
2025-01-20 11:14:05 +0000

    requirements

--- crew_company_search.py
+++ crew_company_search.py
@@ -1,4 +1,9 @@
-__import__('pysqlite3') import sys sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys 
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+import pysqlite3
 import os
 import crewai
 import crewai_tools
